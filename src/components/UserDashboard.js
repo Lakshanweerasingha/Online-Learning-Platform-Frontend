14886360ba896fc1ce73ce5757bd8c9608f24273
lakshanmaduwantha <maduwanthalakshan03@gmail.com>
2024-06-21 16:37:57 +0530

    end check

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { UserEnrollment } from './enrollments/UserEnrollment'; // Adjust the import path as needed
 import { CourseUser } from './courses/CourseUser'; // Adjust the import path as needed
+import { Logout } from './auth/Logout'; // Adjust the import path as needed
 
 export const UserDashboard = () => {
-  return (
-    <div>
-      <h2>User Dashboard</h2>
+    const handleLogout = () => {
+      Logout()
+        .then(() => {
+          // Perform any additional actions after successful logout (e.g., redirect)
+          window.location.href = '/login';
+        })
+        .catch(error => {
+          // Handle any errors here
+          console.error('Failed to logout.', error);
+          // Optionally, you can redirect to login page even if logout fails
+          window.location.href = '/login';
+        });
+    };
+  
+    return (
+      <div>
+              <button onClick={handleLogout}>Logout</button>
+         <h2>User Dashboard</h2>
       <UserEnrollment />
       <CourseUser />
-    </div>
-  );
-};
+      </div>
+    );
+  };
